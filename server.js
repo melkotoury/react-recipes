2807b2e2bb44c3dd953b52658e0d93608cbcf7f4
melkotoury <melkotoury@gmail.com>
2019-02-03 14:22:03 +0200

    feat: add schemas

--- server.js
+++ server.js
@@ -1,4 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config({path: '.env'});
+
+const Recipe = require('./models/Recipe');
+const User = require('./models/User');
+
+// connects to database
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log('DB Connected'))
+    .catch(err => console.error(err));
+
+// Initializes application
+
 const app = express();
 
 const PORT = process.env.PORT || 4444;
